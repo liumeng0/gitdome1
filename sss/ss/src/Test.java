f39efbf636bfd4a4468591c4b74bcd35093a892a
liumeng0 <212948038@qq.com>
2018-06-19 14:31:21 +0800

    2

--- sss/ss/src/Test.java
+++ sss/ss/src/Test.java
@@ -1,8 +1,17 @@
 package src;
 
+
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("test");
+		System.out.println("Test4");
+		mathod();
+		mathodA();
+	}
+	public static void mathod() {
+		System.out.println("test2");
+	}
+	public static void mathodA() {
+		System.out.println("test3");
 	}
 
 }
